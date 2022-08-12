233965f6fffc8cebfc537ef6480eb0185ea2fae9
Yanzd <2223126487@qq.com>
2022-08-12 09:58:20 +0800

    进行js点击函数的封装、点击相对应的体验课程 缩减代码

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+title = document.querySelector('.title')
 newP = document.querySelector('.new')
 category = document.querySelector('.category')
 showData = document.querySelector('.showData')
@@ -18,8 +19,9 @@ month = document.querySelector('.month')
 
 flag = false
 flag1 = false
+// 新员工 隐藏内容显示、图标文件更换 
 newP.onclick=function(){
-    flag = !flag
+    flag = !flag 
     if(flag){
         category.style.display = 'block'
         icon.className ='iconfont icon-xiangxia'
@@ -30,14 +32,20 @@ newP.onclick=function(){
         category.style.display = 'none'
         icon.className ='iconfont icon-xiangzuo'
     }
+    title.innerText = newP.innerText
 }
+
+// 点击童程童创文件切换
 Tcheng.onclick=function(){
     showData.src = './童程新员工入职需知.pdf'
+    title.innerText = Tcheng.innerText
 }
 Tchuang.onclick=function(){
     showData.src = './童创新员工入职需知.pdf'
+    title.innerText = Tchuang.innerText
 }
 
+//体验课相关内容
 experience.onclick=function(){
     flag1 = !flag1
     if(flag1){
@@ -52,53 +60,28 @@ experience.onclick=function(){
     }
 }
 // 课程点击
-// function Click(ele,name){
-//     ele.onclick=function(){
-//         showData.src = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
-//         course.style.display = 'none'
-//         experienceI.className = 'experienceI iconfont icon-arrow_left_fat'
-//         flag1 = false
-    
-//     }
-// }
-python.onclick=function(){
-    showData.src = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
-    course.style.display = 'none'
-    experienceI.className = 'experienceI iconfont icon-arrow_left_fat'
-    flag1 = false
-
+function Click(ele,srcName){
+        ele.onclick=function(){
+            showData.src = srcName
+            course.style.display = 'none'
+            experienceI.className = 'experienceI iconfont icon-arrow_left_fat'
+            flag1 = false
+               title.innerText = ele.innerText
+         }
 }
+// 视频地址
+pythonSrc = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
+wedoSrc = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
+cSrc = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
+spikeSrc  = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
+childSrc  = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
 
-wedo.onclick=function(){
-    showData.src = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
-    course.style.display = 'none'
-    experienceI.className = 'experienceI iconfont icon-arrow_left_fat'
-    flag1 = false
-}
-
-c.onclick=function(){
-    showData.src = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
-    course.style.display = 'none'
-    experienceI.className = 'experienceI iconfont icon-arrow_left_fat'
-    flag1 = false
-
-}
-
-spike.onclick=function(){
-    showData.src = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
-    course.style.display = 'none'
-    experienceI.className = 'experienceI iconfont icon-arrow_left_fat'
-    flag1 = false
-
-}
-
-child.onclick=function(){
-    showData.src = "//player.bilibili.com/player.html?aid=644200934&bvid=BV1SY4y1w7H3&cid=794246857&page=1&high_quality=1&danmaku=0&as_wide=1"
-    course.style.display = 'none'
-    experienceI.className = 'experienceI iconfont icon-arrow_left_fat'
-    flag1 = false
-
-}
+//调用函数进行点击测试
+Click(python,pythonSrc)
+Click(wedo,wedoSrc)
+Click(c,cSrc)
+Click(spike,spikeSrc)
+Click(child,childSrc)
 
 // 周待办
 weeks.onclick=function(){
